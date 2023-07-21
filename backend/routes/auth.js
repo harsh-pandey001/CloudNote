c338ace4650ad8d3be7af71432a9600e9e6665c7
harsh-pandey001 <78418165+harsh-pandey001@users.noreply.github.com>
2023-07-22 00:00:20 +0530

    jwtwebtoken and bcryptjs added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const User = require('../models/User')
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'Harshisgoodboy'
 // create a User using: POST "/api/auth/createuser"//No login required
 
 router.post('/createuser',[
@@ -21,15 +24,26 @@ router.post('/createuser',[
     if (user) {
       return res.status(400).json({error: "Sorry a user with this email already exists"})
     }
-
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password,salt);  
     // create a new user
     user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email ,
-      })
+      });
+
+      const data = {
+        user:{
+          id: user.id
+        }
+      }
+     const authtoken =  jwt.sign(data, JWT_SECRET);
+
    
-      res.json(user)}
+      // res.json(user)
+      res.json({authtoken})
+    }
       catch(error){
         console.error(error.message);
         res.status(500).send("Some error occures");
