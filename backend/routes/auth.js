2958b7d6d9fddb374c058e13f03d49628091cc99
harsh-pandey001 <78418165+harsh-pandey001@users.noreply.github.com>
2023-08-23 23:57:36 +0530

    signup page added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -16,16 +16,17 @@ router.post('/createuser',[
     body('email','Enter a valid E-mail').isEmail(),
     body('password','Password must be atleast 5 characters').isLength({ min: 5 }),
 ], async (req, res)=>{
+  let success = false;
   //if there are errors, return bad request and errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ success, errors: errors.array() });
     }
     //Check whether the user with this email exists already
     try{
     let user = await User.findOne({email: req.body.email}); 
     if (user) {
-      return res.status(400).json({error: "Sorry a user with this email already exists"})
+      return res.status(400).json({success,error: "Sorry a user with this email already exists"})
     }
     const salt = await bcrypt.genSalt(10);
     const secPass = await bcrypt.hash(req.body.password,salt);  
@@ -45,7 +46,8 @@ router.post('/createuser',[
 
    
       // res.json(user)
-      res.json({authtoken})
+      success = true
+      res.json({success,authtoken})
     }
       catch(error){
         console.error(error.message);
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -47,9 +47,7 @@ const Login = (props) => {
             value={credentials.email}
             onChange={onChange}
           />
-          <small id="emailHelp" className="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
+          
         </div>
         <div className="form-group">
           <label htmlFor="exampleInputPassword1">Password</label>
