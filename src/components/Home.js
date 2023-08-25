b17a09cb547c4338148fa45ea2e0369690754ee6
harsh-pandey001 <78418165+harsh-pandey001@users.noreply.github.com>
2023-08-25 12:12:33 +0530

    testing

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,19 +4,20 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Home = (props) => {
   const {showalert} = props;
-  let history = useHistory();
-  useEffect(() => {
-    if(localStorage.getItem('token')){
-      <Notes  showalert = {showalert}/>
-   }
-    else{
-     props.showalert("Please Login or SignUp first", "danger");
-     history.push("/login")
-    }
-    // eslint-disable-next-line
-  }, []);
+  // let history = useHistory();
+  // useEffect(() => {
+  //   if(localStorage.getItem('token')){
+    
+  //  }
+  //   else{
+  //    props.showalert("Please Login or SignUp first", "danger");
+  //    history.push("/login")
+  //   }
+  //   // eslint-disable-next-line
+  // }, []);
   return (
     <>
+      <Notes  showalert = {showalert}/>
     </>
   );
 };
