e9a69fd35cc75233db58b322b63f4de54a47092f
harsh-pandey001 <78418165+harsh-pandey001@users.noreply.github.com>
2023-10-08 16:36:45 +0530

    lastone

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 
 const mongoURl = "mongodb://localhost:27017/inotebook"
+// const mongoURl = process.env.MONGODB_URI
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURl, ()=>{
         console.log("Conneted to mongo Successfully");
     })
 }
+
 
 module.exports = connectToMongo;
